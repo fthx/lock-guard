a8748982e7423879015af2f64b5ee445a375c654
fthx <thirioux@gmail.com>
2025-12-11 09:29:18 +0100

    Use session mode for extensions

--- extension.js
+++ extension.js
@@ -7,18 +7,16 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 
 export default class LockGuardExtension {
-    constructor() {
-        Main.sessionMode.connectObject('updated', () => this._toggleVisibility(), this);
-    }
-
     _toggleVisibility() {
         Main.panel.statusArea.dateMenu.visible = !Main.sessionMode.isLocked;
         Main.panel.statusArea.quickSettings.visible = !Main.sessionMode.isLocked;
     }
 
     enable() {
+        this._toggleVisibility();
     }
 
     disable() {
+        this._toggleVisibility();
     }
 }
